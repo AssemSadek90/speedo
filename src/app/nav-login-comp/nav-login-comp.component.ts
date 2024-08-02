1366a91f56ef627928b8f6a396b4b26b3203e94e
AssemSadek90 <101525092+AssemSadek90@users.noreply.github.com>
2024-08-02 14:00:32 +0300

    Add Login modal, and Register skeleton.

--- src/app/nav-login-comp/nav-login-comp.component.ts
+++ src/app/nav-login-comp/nav-login-comp.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'nav-login-comp',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav-login-comp.component.html',
   styleUrl: './nav-login-comp.component.scss'
 })
 export class NavLoginCompComponent {
-
+  isLoginModalHidden: boolean = true;
+  isRegisterModalHidden: boolean = true;
+  handleOpenLoginModal() {
+    this.isLoginModalHidden = !this.isLoginModalHidden;
+  }
+  handleOpenRegisterModal() {
+    this.isRegisterModalHidden = !this.isRegisterModalHidden;
+  }
 }
--- src/app/navbar-comp/navbar-comp.component.ts
+++ src/app/navbar-comp/navbar-comp.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { NavLoginCompComponent } from '../nav-login-comp/nav-login-comp.component';
 
 @Component({
   selector: 'navbar-comp',
   standalone: true,
-  imports: [NavLoginCompComponent],
+  imports: [NavLoginCompComponent, RouterLink, RouterLinkActive],
   templateUrl: './navbar-comp.component.html',
   styleUrl: './navbar-comp.component.scss'
 })
