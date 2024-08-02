8098eae84bd1b15b324bd3a70a0715fbb583a793
salmaali561 <salma.ali.salem.1920@gmail.com>
2024-08-02 23:47:41 +0200

    3 updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { PaymentHistoryComponent } from './payment-history/payment-history.compo
 import { ChangePasswordComponent } from './change-password/change-password.component';
 import { MyAccountComponent } from './my-account/my-account.component';
 import { MyProfileComponent } from './my-profile/my-profile.component';
+import { UpdateProfileComponent } from './update-profile/update-profile.component';
 
 export const routes: Routes = [
   { path: 'myaccount', component: MyAccountComponent,
@@ -11,8 +12,19 @@ export const routes: Routes = [
       { path: '', redirectTo: 'myprofile', pathMatch: 'full' },
       { path: 'myprofile', component: MyProfileComponent, },
       { path: 'paymenthistory', component: PaymentHistoryComponent },
-      { path: 'settings', component: SettingsComponent },
-      { path: 'changepassword', component: ChangePasswordComponent },
+      { path: 'settings', component: SettingsComponent, 
+        children:[
+          { path: '', redirectTo: 'updateProfile', pathMatch: 'full' },
+          { path: 'updateProfile', component: UpdateProfileComponent},
+          { path: 'changepassword', component: ChangePasswordComponent }
+        ]
+       },
+      { path: 'settings', component:SettingsComponent, 
+        children:[
+         { path: '', redirectTo: 'changepassword', pathMatch: 'full' },
+         { path: 'changepassword', component: ChangePasswordComponent }
+        ]
+       },
       {path: '**', redirectTo: 'myprofile'}
     ]
   },
--- src/app/payment-history/payment-history.component.ts
+++ src/app/payment-history/payment-history.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ViewChildren, QueryList, ElementRef } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-payment-history',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], 
   templateUrl: './payment-history.component.html',
-  styleUrl: './payment-history.component.scss'
+  styleUrls: ['./payment-history.component.scss']
 })
 export class PaymentHistoryComponent {
+  @ViewChildren('textToCopy') textToCopies!: QueryList<ElementRef<HTMLParagraphElement>>;
+  copyIcons: string[] = ['/assets/icons/copy.svg', '/assets/icons/copy.svg']; 
 
+  items = [
+    { accountNumber: '6785 4987 6543' },
+    { accountNumber: '1234 5678 9012' }
+  ];
+
+  copyText(index: number): void {
+    const element = this.textToCopies.toArray()[index].nativeElement;
+    const range = document.createRange();
+    range.selectNode(element);
+
+    window.getSelection()?.removeAllRanges();
+    window.getSelection()?.addRange(range);
+
+    try {
+      document.execCommand('copy');
+      this.copyIcons[index] = '/assets/icons/check mark.svg'; 
+
+      setTimeout(() => {
+        this.copyIcons[index] = '/assets/icons/copy.svg'; 
+      }, 1500);
+    } catch (err) {
+      console.error('Failed to copy text: ', err);
+    }
+
+    window.getSelection()?.removeAllRanges();
+  }
 }
